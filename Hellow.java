53999478373c127c7d96928ffe06b6636dff6e23
Baurens-Torrente Valentin <valentin.baurens-torrente@etu.univ-amu.fr>
2025-01-31 14:19:39 +0100

    added Hellow.java (just a hello world programs in java)

---
+++ Hellow.java
@@ -0,0 +1,5 @@
+public class Hellow {   // Save as "hello_world.java"
+   public static void main(String[] args) {  // Program entry point
+      System.out.println("hello world");    // Print text message
+   }
+}
