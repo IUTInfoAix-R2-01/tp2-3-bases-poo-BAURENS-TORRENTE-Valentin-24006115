d18c302bd0f91190ffc2b8dd32a232c21ab74c39
Baurens-Torrente Valentin <valentin.baurens-torrente@etu.univ-amu.fr>
2025-01-31 17:29:37 +0100

    updated Time (still doesn't work)

--- TP2/Exo1/Time.java
+++ TP2/Exo1/Time.java
@@ -14,34 +14,35 @@ public class Time {
 		return hour;
 	}
 	public void setHour(int hour) {
-		this.hour = hour;
+		this.hour = hour % 24;
+		
 	}
 	public int getMinute() {
 		return minute;
 	}
 	public void setMinute(int minute) {
-		this.minute = minute;
+			this.hour = hour % 60;
 	}
 	public int getSecond() {
 		return second;
 	}
 	public void setSecond(int second) {
-		this.second = second;
+		this.second = second % 60;
 	}
 	public void setTime (int hour, int minute, int second) {
-		this.hour = hour;
-		this.minute = minute;
-		this.second = second;
+		this.hour = hour % 24;
+		this.minute = minute % 60;
+		this.second = second % 60;
 	}
 	public String toString() {
 		return hour + ":" + minute + ":" + second;
 	}
 	public Time nextSecond() {
-		++second;
+		this.setSecond(++second);
 		return this;
 	}
 	public Time previousSecond() {
-		--second;
+		this.setSecond(--second);
 		return this;
 	}
 	
